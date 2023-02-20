e836de08f529c915633d697ca58007f534751924
Herbet Junior <herbetjr@gmail.com>
2023-02-20 18:34:30 -0300

    dsdsd

--- src/views/auth/login/LoginPage.vue
+++ src/views/auth/login/LoginPage.vue
@@ -2,24 +2,19 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar color="warning">
-        <ion-title>AguiaSeg</ion-title>
+
       </ion-toolbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">AguiaSeg</ion-title>
-        </ion-toolbar>
-      </ion-header>
 
       <div id="container" class="container mt-5">
         <img id="main-logo" src="@/assets/img/logo.png" alt="logo">
-        <ion-item class="mt-5 mb-4 login-input" fill="outline">
+        <ion-item class="mt-5 mb-4 login-input" fill="outline" color="warning">
           <ion-label position="floating">Login</ion-label>
           <ion-input placeholder="CPF"></ion-input>
         </ion-item>
-        <ion-item class="mb-4 login-input" fill="outline">
+        <ion-item class="mb-4 login-input" fill="outline" color="warning">
           <ion-label position="floating">Senha</ion-label>
           <ion-input placeholder="Senha"></ion-input>
         </ion-item>
