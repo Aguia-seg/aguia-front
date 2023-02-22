a22bb6d1b9c8e53b9fae9b0547fd024ee3102cb6
João Vitor <joao.vst003@gmail.com>
2023-02-22 16:53:13 -0300

    criação do ClientsPage para a exibição da tabela

--- src/views/clients/ClientsRoute.ts
+++ src/views/clients/ClientsRoute.ts
@@ -5,6 +5,11 @@ const ClientsRoute: Array<RouteRecordRaw> = [
     path: '/clients/register',
     name: 'ClientRegister',
     component: () => import('@/views/clients/clientsregisterpage/ClientsRegisterPage.vue')
+  },
+  {
+    path: '/clients',
+    name: 'ClientShow',
+    component: () => import('@/views/clients/clientspage/ClientsPage.vue')
   }
 ]
 
---
+++ src/views/clients/clientspage/ClientsPage.vue
@@ -0,0 +1,11 @@
+
+<template>
+<ion-page>
+    <SidebarComponent></SidebarComponent>
+    <NavbarComponent></NavbarComponent>
+</ion-page>
+
+</template>
+
+<script lang="ts" src="./ClientsPage.ts"></script>
+<style scoped src="./ClientsPage.css"></style>
