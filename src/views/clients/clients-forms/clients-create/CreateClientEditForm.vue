ce1730daf2d26eac3c380321974ee91884a6b888
João Vitor <joao.vst003@gmail.com>
2023-03-13 17:05:48 -0300

    asdasdads

---
+++ src/views/clients/clients-forms/clients-create/CreateClientEditForm.vue
@@ -0,0 +1,25 @@
+<template>
+    <ion-header>
+    <ion-toolbar>
+      <ion-buttons slot="end">
+        <ion-button color="medium">Sair</ion-button>
+      </ion-buttons>
+      <ion-title>Editar Cliente</ion-title>
+    </ion-toolbar>
+  </ion-header>
+  <ion-content class="ion-padding">
+    <form>
+        <div class="row">
+            <div class="col-10">
+                <ion-item>
+                    <ion-label position="floating">Nome</ion-label>
+                </ion-item>
+            </div>
+        </div>
+    </form>
+  </ion-content>
+</template>
+
+<script lang="ts">
+
+</script>
