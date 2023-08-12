a300b690cb9acf2ec8b18f8877653384bdfc7699
Herbet Junior <herbetjr@gmail.com>
2023-08-12 20:24:03 -0300

    sdsd

--- src/environments/env.ts
+++ src/environments/env.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const request = axios.create({
-    //  baseURL: 'http://localhost:8000/api', //colocar alguma env var, se quiser
-    baseURL: 'https://aguiaseg.atenaagencia.com/aguia-back/public/api',
+     baseURL: 'http://localhost:8000/api', //colocar alguma env var, se quiser
+    // baseURL: 'https://aguiaseg.atenaagencia.com/aguia-back/public/api',
  });
 
  const  headers = {
